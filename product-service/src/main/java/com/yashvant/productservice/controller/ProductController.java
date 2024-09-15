4c12a18997a603494b43b8424f194d4e691fbc14
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-15 23:09:12 +0530

    writing product-service

--- product-service/src/main/java/com/yashvant/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/yashvant/productservice/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.yashvant.productservice.controller;
 
 import com.yashvant.productservice.dto.ProductRequest;
+import com.yashvant.productservice.dto.ProductResponse;
 import com.yashvant.productservice.model.Product;
 import com.yashvant.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,13 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- product-service/src/main/java/com/yashvant/productservice/service/ProductService.java
+++ product-service/src/main/java/com/yashvant/productservice/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.yashvant.productservice.service;
 
 import com.yashvant.productservice.dto.ProductRequest;
+import com.yashvant.productservice.dto.ProductResponse;
 import com.yashvant.productservice.model.Product;
 import com.yashvant.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +18,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public Product createProduct(ProductRequest productRequest) {
+    public ProductResponse createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
@@ -23,6 +26,22 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product created successfully.");
-        return product;
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice()
+        );
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()
+                )).toList();
     }
 }
