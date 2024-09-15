ee89d0f28918d9fab2edca71de9469b07f2910d8
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-15 23:21:32 +0530

    Writing tests 📝 for product-service

--- product-service/src/test/java/com/yashvant/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/yashvant/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package com.yashvant.productservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "Product 1",
+                    "description": "Product 1 description",
+                    "price": 100
+                }
+                """;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Product 1"))
+                .body("description", Matchers.equalTo("Product 1 description"))
+                .body("price", Matchers.equalTo(100));
     }
 
 }
