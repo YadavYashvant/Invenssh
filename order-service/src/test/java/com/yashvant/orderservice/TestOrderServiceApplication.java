d961c080cd4acf8732159cb79cec3bfbc5cd623a
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-17 22:30:30 +0530

    Created order service

---
+++ order-service/src/test/java/com/yashvant/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.yashvant.orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
